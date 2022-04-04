974834a8220dec51165c75bfbd3ce9bec7c32b67
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-04-04 12:52:42 +0530

    Parsing variables from URLs (querys)

--- Topic Wise/Intro to NodeJS and NPM/1-node-farm/index.js
+++ Topic Wise/Intro to NodeJS and NPM/1-node-farm/index.js
@@ -26,11 +26,10 @@ const dataObj = JSON.parse(data);
 
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    const pathName = req.url;
+    const { query, pathname } = url.parse(req.url, true);
 
     //Overview Page
-    if (pathName === '/' || pathName === '/overview') {
+    if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, { 'Content-type': 'text/html' });
 
 
@@ -40,12 +39,15 @@ const server = http.createServer((req, res) => {
     }
 
     //Product Page
-    else if (pathName === '/product') {
-        res.end('This is the product')
+    else if (pathname === '/product') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
     }
 
     //API
-    else if (pathName === '/api') {
+    else if (pathname === '/api') {
         res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(data);
     }

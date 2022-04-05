43b4f05276e1a6e5aba798ba36a9b31871b8de83
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-04-05 17:06:08 +0530

    handling POST requests

--- Topic Wise/2-Express and API/natours/app.js
+++ Topic Wise/2-Express and API/natours/app.js
@@ -3,8 +3,10 @@ const express = require('express');
 
 const app = express();
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+app.use(express.json());
 
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
@@ -15,6 +17,24 @@ app.get('/api/v1/tours', (req, res) => {
         }
     })
 });
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body)
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+});
+
 
 const port = 3000;
 app.listen(port, () => {

8c9e04fc3c8c35ec10131ecd21ccfaa7d9b3c70a
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-04-05 16:50:09 +0530

    handling get requests and starting api development

--- Topic Wise/2-Express and API/natours/app.js
+++ Topic Wise/2-Express and API/natours/app.js
@@ -1,19 +1,20 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+
+app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
-        message: 'Hello from the outside',
-        app: 'natours'
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours    //same as tours: tours
+        }
     })
 });
-
-app.post('/', (req, res) => {
-    res.send('You can send to this input and the server is here for you to listen, you are not alone');
-});
-
-
 
 const port = 3000;
 app.listen(port, () => {

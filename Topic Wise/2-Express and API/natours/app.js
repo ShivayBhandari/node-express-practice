396a928d2eb6c5c05a05406b1e413a9f0a35eb00
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-04-06 20:35:23 +0530

    responding to URL paramaters

--- Topic Wise/2-Express and API/natours/app.js
+++ Topic Wise/2-Express and API/natours/app.js
@@ -18,6 +18,27 @@ app.get('/api/v1/tours', (req, res) => {
     })
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+    // console.log(req.params);
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'invalid ID'
+        });
+    }
+
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: tour
+        }
+    });
+});
+
 app.post('/api/v1/tours', (req, res) => {
     // console.log(req.body);
 

71389878ecd63fe17f7e668fa8815d80a339bd42
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-04-10 17:47:15 +0530

    refactoring for MVC

--- Topic Wise/2-Express and API/natours/controllers/tourController.js
+++ Topic Wise/2-Express and API/natours/controllers/tourController.js
@@ -1,58 +1,37 @@
-const fs = require('fs');
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
-
-exports.checkId = (req, res, next, val) => {
-    console.log(`tour id is ${val}`);
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'invalid ID'
-        });
-    }
-    next();
-};
+const Tour = require('./../models/tourModel');
 
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
     res.status(200).json({
         status: 'success',
         requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours    //same as tours: tours
-        }
+        // results: tours.length,
+        // data: {
+        //     tours    //same as tours: tours
+        // }
     });
 };
 
 exports.getTour = (req, res) => {
     const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
+    // const tour = tours.find(el => el.id === id);
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: tour
-        }
-    });
+    // res.status(200).json({
+    //     status: 'success',
+    //     data: {
+    //         tour: tour
+    //     }
+    // });
 };
 
 exports.createTour = (req, res) => {
-    // console.log(req.body);
-
-
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body)
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        });
+    res.status(201).json({
+        status: 'success',
+        // data: {
+        //     tour: newTour
+        // }
     });
-};
+}
 
 exports.updateTour = (req, res) => {
 
--- Topic Wise/2-Express and API/natours/routes/tourRoutes.js
+++ Topic Wise/2-Express and API/natours/routes/tourRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const tourController = require('./../controllers/tourController')
 
-router.param('id', tourController.checkId);
+// router.param('id', tourController.checkId);
 
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
